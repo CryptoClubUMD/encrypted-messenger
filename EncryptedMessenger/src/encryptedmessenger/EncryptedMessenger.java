e8c3be498988f43643f1cbbd2709f3abc24dbba3
Jakson Kallio <jakson@protonmail.com>
2017-11-16 18:23:18 -0600

    Base files

---
+++ EncryptedMessenger/src/encryptedmessenger/EncryptedMessenger.java
@@ -0,0 +1,18 @@
+package encryptedmessenger;
+
+/**
+ *
+ * @author Jakson R. Kallio
+ * @author Evan
+ * @author Naseem
+ */
+public class EncryptedMessenger {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
